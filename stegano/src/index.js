0d4716c34a12668e23392e1a40ffa60dce99298a
alexander-litvinovich <litvinovich.alexander@gmail.com>
2023-09-23 18:47:45 +0300

    feat: modifying image

--- stegano/src/index.js
+++ stegano/src/index.js
@@ -6,8 +6,19 @@ const png = require('pngjs').PNG;
 const loadedPNG = png.sync.read(fs.readFileSync('image.png'));
 log("Metadata:", loadedPNG);
 
+// Modifying the image
+loadedPNG.data = loadedPNG.data.map((value, index) => {
+    let newValue;
+
+    if (index % 4 !== 3) {
+        newValue = 255 - value;
+    } else newValue = value;
+
+    return newValue;
+})
+
 // Write PNG file
 const options = { colorType: loadedPNG.colorType };
-const outputBuffer = png.sync.write(data, options);
+const outputBuffer = png.sync.write(loadedPNG, options);
 
 fs.writeFileSync('image-out.png', outputBuffer);
