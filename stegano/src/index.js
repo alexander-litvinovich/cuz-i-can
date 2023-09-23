cd9fd56087516233ca489f59cc8f82b02687d222
alexander-litvinovich <litvinovich.alexander@gmail.com>
2023-09-23 20:08:55 +0300

    upd: invert function

--- stegano/src/index.js
+++ stegano/src/index.js
@@ -7,18 +7,26 @@ const loadedPNG = png.sync.read(fs.readFileSync('image.png'));
 log("Metadata:", loadedPNG);
 
 // Modifying the image
-loadedPNG.data = loadedPNG.data.map((value, index) => {
-    let newValue;
-
-    if (index % 4 !== 3) {
-        newValue = 255 - value;
-    } else newValue = value;
-
-    return newValue;
-})
+loadedPNG.data = invertImage(loadedPNG.data);
 
 // Write PNG file
 const options = { colorType: loadedPNG.colorType };
 const outputBuffer = png.sync.write(loadedPNG, options);
 
 fs.writeFileSync('image-out.png', outputBuffer);
+
+// Functions
+
+// Function to invert pixel values
+function invertImage(data) {
+    const invertedData = data.map((value, index) => {
+        let newValue;
+
+        if (index % 4 !== 3) newValue = 255 - value;
+        else newValue = value;
+
+        return newValue;
+    })
+
+    return invertedData;
+}
