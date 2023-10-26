8029270cb0dc72d6186cb4d0cdb1022496fc5539
alexander-litvinovich <litvinovich.alexander@gmail.com>
2023-10-26 10:36:28 +0300

    fix sprite playback

--- gravitygame/src/GameScene.js
+++ gravitygame/src/GameScene.js
@@ -8,10 +8,11 @@ class Asteroid extends Phaser.GameObjects.Sprite {
         let x = Math.random() * scene.game.canvas.width; //TBD Fix this
 
 
-        super(scene, x, y, 'asteroid')
+        super(scene, x, y, 'asteroid_sprite')
         scene.add.existing(this);
 
-        scene.physics.world.enableBody(this)
+        scene.physics.world.enableBody(this);
+        this.setScale(0.5);
         this.body.velocity.y = 50 + 90 * Math.random();
         this.body.velocity.x = 5 * Math.random();
         this.body.bounce.set(1);
@@ -21,10 +22,24 @@ class Asteroid extends Phaser.GameObjects.Sprite {
         this.gameHeight = scene.game.canvas.height;
 
         scene.events.on('update', (time, delta) => { this.update(time, delta) });
-        console.log(scene);
+        scene.events.on('create', () => this.create());
+        console.log(this);
+
+        this.anims.play('spin', true);
+    }
+    preload() {
+
+    }
+
+    create() {
+        console.log("create");
+
+
     }
 
     update() {
+
+
         if (this.y > this.gameHeight || this.y < this.gameHeight / -3) {
             this.destroy();
         }
@@ -52,14 +67,52 @@ export default class GameScene extends Phaser.Scene {
         this.load.image('space', 'assets/background.png');
         this.load.image('player', 'assets/player.png');
         this.load.image('asteroid', 'assets/asteroid.png');
+        this.load.spritesheet('asteroid_sprite',
+            'assets/asteroid_sprite.png',
+            { frameWidth: 128, frameHeight: 128 }
+        );
+        this.load.spritesheet('player_sprite',
+            'assets/player_sprite.png',
+            { frameWidth: 96 * 2, frameHeight: 87 * 2 }
+        );
     }
 
     create() {
         this.bg = this.add.tileSprite(300, 700, 2024, 2024, 'space');
         // this.bg.setOrigin(0.5, 0.90);
         this.ship = this.physics.add.sprite(300, 700, 'player_sprite');
+        this.ship.setScale(0.5);
         this.ship.setImmovable(true);
         this.ship.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'spin',
+            frames: this.anims.generateFrameNumbers('asteroid_sprite', { start: 0, end: 31 }),
+            frameRate: 24,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('player_sprite', { start: 0, end: 2 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player_sprite', { start: 3, end: 5 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player_sprite', { start: 6, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
         this.cursorKeys = this.input.keyboard.createCursorKeys();
 
         this.asteroids = this.add.group();
@@ -98,7 +151,6 @@ export default class GameScene extends Phaser.Scene {
                 this.bg.rotation = (this.bg.rotation / 2);
         }
 
-        this.ship.setVelocityX(0);
 
 
 
@@ -106,26 +158,31 @@ export default class GameScene extends Phaser.Scene {
             this.ship.setVelocityX(-VELOCITY_SHIFT);
             this.bg.rotation += VELOCITY_ANGLE;
             this.asteroids.rotation -= 10 * VELOCITY_ANGLE;
+            this.ship.anims.play('left', true)
         }
 
 
 
         if (this.cursorKeys.right.isDown) {
             this.ship.setVelocityX(VELOCITY_SHIFT);
-
             this.bg.rotation -= VELOCITY_ANGLE;
             this.asteroids.rotation += 10 * VELOCITY_ANGLE;
+            this.ship.anims.play('right', true)
         }
 
+        if (this.cursorKeys.left.isUp && this.cursorKeys.right.isUp) {
+            this.ship.setVelocityX(0);
+            this.ship.anims.play('idle', true)
+        }
 
         this.physics.collide(this.ship, this.asteroids, (ship, asteroid) => {
             console.log('hit', this.lifes);
             this.lifes--;
-            if (this.lifes <= 0) {
-                this.scene.start('game-over-scene');
-            }
+            // if (this.lifes <= 0) {
+            //     this.scene.start('game-over-scene');
+            // }
             // this.ship.destroy();
-            asteroid.destroy();
+            // asteroid.destroy();
             // this.scene.start('game-scene');
         });
         this.physics.collide(this.asteroids);
